4dfd8d7edbe2f9b2b01f79427a246fcc00d5650c
joamag <joamag@hive.pt>
2018-03-19 18:53:17 +0000

    small fixes

--- static/js/components/store.js
+++ static/js/components/store.js
@@ -1,1 +1,10 @@
 // TODO: put the code related with the store view !!! (mutiple store views should compose the panel)
+
+Vue.component("store", {
+    template: "<div class=\"store\">This is a store/li>",
+    data: {
+        name: "Store",
+        sales: "12,000 EUR",
+        isMain: False
+    }
+});
