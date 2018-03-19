682fd0876de700cf461c4e859ea710af2a9a9b49
joamag <joamag@hive.pt>
2018-03-19 19:03:44 +0000

    initial store component

--- static/js/components/store.js
+++ static/js/components/store.js
@@ -1,10 +1,12 @@
 // TODO: put the code related with the store view !!! (mutiple store views should compose the panel)
 
 Vue.component("store", {
-    template: "<div class=\"store\">This is a store/li>",
-    data: {
-        name: "Store",
-        sales: "12,000 EUR",
-        isMain: False
+    template: "<div class=\"store\">{{ name }}</div>",
+    data: () => {
+        return {
+            name: "Store",
+            sales: "12,000 EUR",
+            isMain: false
+        };
     }
 });
