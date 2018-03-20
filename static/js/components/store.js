f9075a4bc2fe1228d88a4732597c64a342922d60
joamag <joamag@hive.pt>
2018-03-20 16:49:56 +0000

    new store code

--- static/js/components/store.js
+++ static/js/components/store.js
@@ -1,11 +1,14 @@
 Vue.component("store", {
     template: "<div class=\"store\">{{ store ? store.name : name }}</div>",
-    props: ["store"],
-    data: () => {
-        return {
-            name: "Store",
-            sales: "12,000 EUR",
-            isMain: false
-        };
+    props: {
+        store: {
+            type: Object,
+            default: {
+                name: "Example Store",
+                email: "example@store.com",
+                sales: "12,000 EUR",
+                isMain: false
+            }
+        }
     }
 });
