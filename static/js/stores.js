3529f618944600c947fa688481160c0dc4995c37
joamag <joamag@gmail.com>
2018-03-20 23:18:58 +0000

    new fixes

--- static/js/stores.js
+++ static/js/stores.js
@@ -9,23 +9,23 @@ const stores = function() {
             remote: function() {
                 const timestamp = parseInt(Date.parse(new Date().toUTCString()) / 1000)
                 this.$http.get("https://ldj.frontdoorhd.com/omni/sale_snapshots/stats.json", {
-                    params: {
-                        date: timestamp,
-                        has_global: "True",
-                        output: "simple",
-                        span: 7,
-                        unit: "day"
-                    }
-                })
+                        params: {
+                            date: timestamp,
+                            has_global: "True",
+                            output: "simple",
+                            span: 7,
+                            unit: "day"
+                        }
+                    })
                     .then(response => {
                         debugger;
                         /*this.message = "Success";
                         this.details = "Users loaded from remote data source";
                         this.users = response.data;*/
                     }, response => {
-                       /* this.message = "Error";
-                        this.details = JSON.stringify(response);
-                        this.users = [];*/
+                        /* this.message = "Error";
+                         this.details = JSON.stringify(response);
+                         this.users = [];*/
                     });
             }
         }
