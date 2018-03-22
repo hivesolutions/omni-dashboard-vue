a351c7f9af407a5e83a77553198eed635f81948d
joamag <joamag@hive.pt>
2018-03-22 12:04:57 +0000

    better vue loading

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
         filename: "bundle.js",
         library: "OmniDashboard"
     },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: "vue-loader"
+        }]
+    },
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.esm.js"
+        },
+        extensions: ["*", ".js", ".vue", ".json"]
+    },
     devtool: "inline-source-map"
 };
