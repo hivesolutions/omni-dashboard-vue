c9070dbf578562c7fae8b44b41b37727407212cd
joamag <joamag@hive.pt>
2018-06-13 18:40:35 +0100

    new eslint config hive

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,5 @@
 module.exports = {
     "extends": [
-        "standard",
-        "plugin:vue/essential"
-    ],
-    "plugins": ["mocha", "vue"],
-    "rules": {
-        "indent": ["warn", 4, {
-            SwitchCase: 1
-        }],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-        "space-before-function-paren": ["error", {
-            "anonymous": "never",
-            "named": "never",
-            "asyncArrow": "always"
-        }],
-        "mocha/no-exclusive-tests": "error",
-        "no-debugger": "warn"
-    },
-    "env": {
-        "jasmine": true
-    }
+        "hive"
+    ]
 };
