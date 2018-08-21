0e7f178d67ffc6a24d85c123fbf3d6f25e5e3f0d
joamag <joamag@hive.pt>
2018-08-21 14:18:47 +0100

    support for multiple gradients

--- index.html.tpl
+++ index.html.tpl
@@ -6,7 +6,8 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1" />
         <title><%= htmlWebpackPlugin.options.title %></title>
     </head>
-    <body class="center cool cool-color cool-gradient-crazy" onload="OmniDashboard.start();">
+    <body class="center cool cool-color" onload="OmniDashboard.start();"
+          data-gradients="cool-gradient-green,cool-gradient-crazy">
         <noscript>Sorry, your browser does not support JavaScript.</noscript>
         <div id="app" class="app app-stores">
             <overlay ref="overlay"></overlay>
--- src/app.js
+++ src/app.js
@@ -11,11 +11,22 @@ import "./assets/css/layout.css";
 export const globals = {};
 
 export const start = function() {
+    startBody();
+    startApp();
+    install();
+};
+
+export const startBody = function() {
+    const colors = document.body.dataset.gradients.split(",");
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    document.body.classList.add(color);
+};
+
+export const startApp = function() {
     const element = document.getElementById("app");
     if (element.classList.contains("app-stores")) {
         globals.app = stores();
     }
-    install();
 };
 
 export const install = function() {
