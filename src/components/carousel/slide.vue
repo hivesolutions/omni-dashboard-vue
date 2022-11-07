c1e6010eae2026b80ca82c83e921a931a2369ea4
João Magalhães <joamag@gmail.com>
2022-11-07 17:30:21 +0000

    feat: initial working carousel

--- src/components/carousel/slide.vue
+++ src/components/carousel/slide.vue
@@ -1,14 +1,30 @@
 <template>
-    <div class="slide">
+    <div class="slide" v-bind:style="style">
         <slot />
     </div>
 </template>
 
 <style scoped>
+.slide {
+    display: inline-block;
+}
 </style>
 
 <script>
 export const Slide = {
+    props: {
+        width: {
+            type: Number,
+            default: undefined
+        }
+    },
+    computed: {
+        style() {
+            const base = {};
+            if (this.width) base.width = `${this.width}px`;
+            return base;
+        }
+    }
 };
 
 export default Slide;
